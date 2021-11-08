ddf1faaf496e7ef946741c6fce0df81bbcae40c6
CornerPin <CornerPin@DESKTOP-OOHTGBK>
2021-11-08 13:20:21 +0300

    Add project files.

---
+++ BrideAndGroom/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BrideAndGroomLibrary;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GoodLuck agency = new();
+
+			//agency.AddPerson(new BrideAndGroom());
+
+			throw new NotImplementedException();
+		}
+	}
+}
