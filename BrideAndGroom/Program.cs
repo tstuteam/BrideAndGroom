e81481bfb146dfa1dae1a614d3bd214e5d44c1d3
CornerPin <7669876+CornerPin@users.noreply.github.com>
2021-11-16 14:16:47 +0300

    Updated console app.

--- BrideAndGroom/Program.cs
+++ BrideAndGroom/Program.cs
@@ -5,58 +5,61 @@ namespace BrideAndGroom
 {
     internal static class Program
     {
+        private static GoodLuck Agency = new(false);
+        private static void Main()
+        {
+            Test();
+        }
+
         private static void Test()
         {
-            GoodLuck agency = new();
             // невеста
-            var bride = new BrideAndGroomLibrary.BrideAndGroom("Lilya Brik",
+            var bride = new BrideAndGroomLibrary.BrideAndGroom("lilya@yandex.ru",
+                "123123",
+                "Lilya Brik",
                 Gender.Female,
                 Properties.Kind | Properties.Young,
                 Properties.Employed | Properties.Rich
             );
 
             // жених
-            var groom1 = new BrideAndGroomLibrary.BrideAndGroom("Vladimir Mayakovsky",
+            var groom1 = new BrideAndGroomLibrary.BrideAndGroom("vladimir@yandex.ru",
+                "5555555555555", 
+                "Vladimir Mayakovsky",
                 Gender.Male,
                 Properties.Employed,
                 Properties.Kind);
             // жених
-            var groom2 = new BrideAndGroomLibrary.BrideAndGroom(
+            var groom2 = new BrideAndGroomLibrary.BrideAndGroom("noname@gmail.com",
+                "@*4asdjk42@!",
                 "Vladimir Noname",
                 Gender.Male,
                 Properties.None,
                 Properties.Young);
             // жених
-            var groom3 = new BrideAndGroomLibrary.BrideAndGroom("Esenin",
+            var groom3 = new BrideAndGroomLibrary.BrideAndGroom("esenin@yandex.ru",
+                "d98@@91!123",
+                "Esenin",
                 Gender.Male,
                 Properties.Employed | Properties.Rich,
                 Properties.Kind);
 
-            agency.AddPerson(bride);
-            agency.AddPerson(groom1);
-            agency.AddPerson(groom2);
-            agency.AddPerson(groom3);
-
-            Console.WriteLine(agency.FindBestPair(bride));
-        }
-
-        public static void workFile1()
-        {
-            string Email = "waeff@fvw.eee";
-            string key = "1654";
-            var groom1 = new BrideAndGroomLibrary.BrideAndGroom("Esenin",
+            var groom4 = new BrideAndGroomLibrary.BrideAndGroom("waeff@fvw.eee",
+                "1653",
+                "Esenin",
                 Gender.Male,
                 Properties.Employed | Properties.Rich,
                 Properties.Kind);
 
-            Email = WorkFile.file_add(Email, key, groom1);
-            var groom2 = WorkFile.ReadFile(Email, key);
-        }
-        private static void Main()
-        {
-            //Test();
-            workFile1();
-            WorkFile.ReadFaills();
+            Agency.AddPerson(bride);
+            Agency.AddPerson(groom1);
+            Agency.AddPerson(groom2);
+            Agency.AddPerson(groom3);
+            Agency.AddPerson(groom4);
+
+            Agency.DB.UpdateData(true);
+
+            Console.WriteLine(Agency.FindBestPair(bride));
         }
     }
 }
